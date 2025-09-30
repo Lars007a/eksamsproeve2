c6daf04c697efbba6653c40d9a3750bd8dcbd1c0
Lars <laarrss@protonmail.com>
2025-09-30 07:54:43 +0200

    navbar fra i går

--- src/comps/navbar/navbar.jsx
+++ src/comps/navbar/navbar.jsx
@@ -1,5 +1,89 @@
 import styles from "./navbar.module.css";
+import logo from "../../assets/logo.png";
+import { Link, NavLink } from "react-router-dom";
+import cartImg from "../../assets/basket_icon.png";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
-  return;
+  const [active, setActive] = useState(false);
+  const [scrollActive, setScrollActive] = useState(false);
+
+  const toggle = (event) => {
+    if (active) {
+      setActive(false);
+    } else {
+      setActive(true);
+    }
+  };
+
+  const handleScroll = (event) => {
+    if (window.scrollY > 400) {
+      setScrollActive(true);
+    } else {
+      setScrollActive(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); /* køre når component bliver mountet, og bliver renderet første gang kan man sige. */
+
+  return (
+    <nav className={`${styles.nav} ${scrollActive ? styles.scroll : ""}`}>
+      <div className={`container ${styles.content}`}>
+        {/* logoet */}
+        <a to="/">
+          <div
+            className={styles.logoImg}
+            style={{ backgroundImage: `url(${logo})` }}
+          />
+        </a>
+        {/* menu knapperne, som kurv og hamburger knappen. */}
+        <div className={styles.btns}>
+          <a href="#">
+            <div
+              className={styles.cartImg}
+              style={{ backgroundImage: `url(${cartImg})` }}
+            />
+          </a>
+          <div
+            className={`${styles.hamburger} ${active ? styles.active : ""}`}
+            onClick={toggle}
+          >
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+          </div>
+        </div>
+        {/* selve menuen der åbner op */}
+        <div className={`${styles.activeMenu} ${active ? styles.active : ""}`}>
+          <ul>
+            <li>
+              <NavLink className={"titleFont"} to="/">
+                Forside
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={"titleFont"} to="#">
+                Personalet
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={"titleFont"} to="#">
+                Kontakt
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={"titleFont"} to="#">
+                Kurv
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,14 @@ import "@fontsource/kurale";
 import "./index.css";
 import Navbar from "./comps/navbar/navbar.jsx";
 import Header from "./comps/header/header.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Navbar />
-    <Header textOne={"Den"} textTwo={"Glade"} textThree={"Skorbe"} />
+    <BrowserRouter>
+      <Navbar />
+      <Header textOne={"Den"} textTwo={"Glade"} textThree={"Skorbe"} />
+      <div style={{ height: "100vh" }}></div>
+    </BrowserRouter>
   </StrictMode>
 );
