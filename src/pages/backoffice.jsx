c23f0440458ffd265977e5d180604888e92751ac
Lars007a <laarrss@protonmail.com>
2025-12-03 23:10:07 +0100

    ændret api endpoint.

--- src/pages/backoffice.jsx
+++ src/pages/backoffice.jsx
@@ -5,11 +5,17 @@ import BackofficeNav from "../comps/backofficeComps/backofficeNav/backofficeNav"
 export default function Backoffice({}) {
 
 
+  const dontSHow = useState(true);
 
   const [page, setPage] = useState(<BackofficeEmployees></BackofficeEmployees>)
 
   return <>
+  {
+    dontSHow ? "" :
+    <>
   <BackofficeNav setter={setPage} />
   {page}
+  </>
+  }
   </>;
 }
